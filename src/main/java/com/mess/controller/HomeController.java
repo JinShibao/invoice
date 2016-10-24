4dfb27a22613234516740f7e21c36a68b16a9503
jinshibao <jinshibao@lianjia.com>
2016-10-24 16:38:54 +0800

    add error page

--- src/main/java/com/mess/controller/HomeController.java
+++ src/main/java/com/mess/controller/HomeController.java
@@ -1,17 +1,18 @@
 package com.mess.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by jinshibao on 2016/10/14.
  */
 
-@RestController
+@Controller
 public class HomeController {
+
     @RequestMapping("/")
-    public ModelAndView home() {
-        return new ModelAndView("/index");
+    public String home() {
+        return "index";
     }
+
 }
